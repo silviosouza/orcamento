4d4269b36d3a54c50c3674595450a5ee0b91d155
silviosouza <silvioalex.souza@gmail.com>
2025-10-07 13:27:06 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 <script type="module">
-  import { createClient } from '@supabase/supabase-js';
+  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 </script>
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
